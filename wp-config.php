823a5725295e7657a2935d117e6f78912bc7260a
pramod08 <spramod08@gmail.com>
2016-07-21 18:14:28 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -25,7 +25,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
-define('DB_HOST', '192.168.1.58');
+define('DB_HOST', '192.168.1.58'); # give your mysql client HOST IP
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
